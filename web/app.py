c60c60094aad98273e461f99a78cd4401d7c80fc
Morgan Strom <morgan.e.strom@gmail.com>
2017-05-10 05:47:04 +0200

    changed indentation

--- web/app.py
+++ web/app.py
@@ -13,49 +13,52 @@ app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'mysql_1'
 mysql.init_app(app)
 
+
 @app.route('/')
 def main():
     return render_template('index.html')
 
+
 @app.route('/showSignUp')
 def showSignUp():
-	return render_template('signup.html')
+    return render_template('signup.html')
+
 
 @app.route('/signUp', methods=['POST'])
 def signUp():
-	"""Code for creating a user"""
+    """Code for creating a user"""
 
-	try:
-		# Read the posted values from the UI
-		_name = request.form['inputName']
-		_email = request.form['inputEmail']
-		_password = request.form['inputPassword']
+    try:
+        # Read the posted values from the UI
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-		# Validate the received values
-		if _name and _email and _password:
-			# All Good, let's call MySQL
+        # Validate the received values
+        if _name and _email and _password:
+            # All Good, let's call MySQL
 
-			conn = mysql.connect()
-			cursor = conn.cursor()
-			_hashed_password = generate_password_hash(_password)
-			cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
-			data = cursor.fetchall()
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+            data = cursor.fetchall()
 
-			if len(data) is 0:
-				conn.commit()
-				return json.dumps({'message':'User created successfully !'})
-			else:
-				return json.dumps({'error':str(data[0])})
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message': 'User created successfully !'})
+            else:
+                return json.dumps({'error': str(data[0])})
 
-		else:
-			return json.dumps({'html':'<span>Enter the required fields</span>'})
-	except Exception as e:
-		return json.dumps({'error':str(e)})
+        else:
+            return json.dumps({'html': '<span>Enter the required fields</span>'})
+    except Exception as e:
+        return json.dumps({'error': str(e)})
 
-	finally:
-		cursor.close()
-		conn.close()
+    finally:
+        cursor.close()
+        conn.close()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0') #Which host?
+    app.run(debug=True, host='0.0.0.0')  # Which host?
