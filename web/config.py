7796a27a542897c6d216dafeac7c382cbfea88fb
Morgan Strom <morgan.e.strom@gmail.com>
2017-06-18 11:25:41 +0200

    moving mysql configurations to .env file

--- web/config.py
+++ web/config.py
@@ -5,8 +5,8 @@ import os
 class BaseConfig(object):
     SECRET_KEY = os.environ['SECRET_KEY']
     DEBUG = os.environ['DEBUG']
-    MYSQL_DATABASE_DB = os.environ['DB_NAME']
-    MYSQL_DATABASE_USER = os.environ['DB_USER']
-    MYSQL_DATABASE_PASSWORD = os.environ['DB_PASS']
+    MYSQL_DATABASE_DB = os.environ['MYSQL_DATABASE']
+    MYSQL_DATABASE_USER = os.environ['MYSQL_USER']
+    MYSQL_DATABASE_PASSWORD = os.environ['MYSQL_ROOT_PASSWORD']
     MYSQL_DATABASE_HOST = os.environ['DB_HOST']
     # DB_PORT = os.environ['DB_PORT']
