fdd49ee17ca22f2b63d8d51aa0debebfb7bcba3f
Morgan Strom <morgan.e.strom@gmail.com>
2017-11-05 21:56:24 +0100

    Adding documentation and tests for IRT methods

--- web/irt.py
+++ web/irt.py
@@ -15,7 +15,7 @@ def quadrature_points(k=33):
     return np.linspace(-4, 4, k)
 
 
-def prior(theta, mean=0, sd=1):
+def quadrature_weights(theta, mean=0, sd=1):
     """
     Prior with normal distribution, returning probability mass function
     at each quadrature point in theta.
@@ -31,29 +31,55 @@ def prior(theta, mean=0, sd=1):
 def p_correct(theta, a, b):
     """
     Probability of answering a question of a certain difficulty and discrimination
-    correctly, given the individual's ability theta.
+    correctly, given an array of possible theta quadrature points.
     :param theta: Array of quadrature points along ability scale
     :param a: double representing item discrimination
     :param b: double representing item difficulty
-    :return: Array of probabilities for answering correctly for each theta
+    :return: Array of probabilities for answering correctly for each quadrature point
     """
     z = a * (theta - b)
     return 1.0 / (1 + np.exp(-z))
 
 
 def likelihood(theta, a, b, x):
-    #a = np.repeat(1.0, 5)
-    #b = np.array([-2.155, -0.245, 0.206, 0.984, 1.211])
-    #x = np.array([1, 1, 0, 0, 0])
-    p = np.zeros((len(theta), len(x)))
-    for i in np.arange(len(x)):
-        p_ = p_correct(theta, a[i], b[i])
-        p[:, i] = np.power(p_, x[i]) * np.power(1 - p_, 1 - x[i])
-    return np.prod(p, axis = 1)
+    """
+    Likelihood of an observed set of k responses.
+    :param theta: Array of quadrature points along ability scale
+    :param a: Array of k question discrimination parameters
+    :param b: Array of k question difficulty parameters
+    :param x: Array of k question scores (1 or 0)
+    :return: Array of likelihood values for each quadrature point in theta
+    """
+    # Todo: Check that a, b and x have all the same length
+    # Todo: Get rid of for-loop, extend p_correct to handle arrays of a and b
+    if (type(x).__name__ == 'int'):
+        p_ = p_correct(theta, a, b)
+        return np.power(p_, x) * np.power(1 - p_, 1 - x)
+    else:
+        p = np.zeros((len(theta), len(x)))
+        for i in np.arange(len(x)):
+            p_ = p_correct(theta, a[i], b[i])
+            p[:, i] = np.power(p_, x[i]) * np.power(1 - p_, 1 - x[i])
+        return np.prod(p, axis = 1)
 
 
-def eap(theta, L, A):
-    return np.sum(theta * L * A) / np.sum(L * A)
+def eap(theta, L, theta_w):
+    """
+    Expected A Posteriori estimate of individual ability.
+    :param theta: Array of quadrature points along ability scale
+    :param L: Array of likelihood values for each quadrature point
+    :param theta_w: Prior quadrature weights for each quadrature point
+    :return: Double representing the mean of the posterior distribution
+    """
+    return np.sum(theta * L * theta_w) / np.sum(L * theta_w)
 
-def eapSD(theta_hat, theta, L, A):
-    return np.sqrt(np.sum(np.power(theta - theta_hat, 2) * L * A) / np.sum(L * A))
+def eap_psd(theta_hat, theta, L, theta_w):
+    """
+    Posterior Standard Deviation of EAP estimate of individual ability.
+    :param theta_hat: Double representing EAP estimate of individual ability.
+    :param theta: Array of quadrature points along ability scale
+    :param L: Array of likelihood values for each quadrature point
+    :param theta_w: Prior quadrature weights for each quadrature point
+    :return: Double representing the estimated posterior standard deviation
+    """
+    return np.sqrt(np.sum(np.power(theta - theta_hat, 2) * L * theta_w) / np.sum(L * theta_w))
--- web/test/test_irt.py
+++ web/test/test_irt.py
@@ -5,11 +5,96 @@ import numpy as np
 class TestIrtMethods(unittest.TestCase):
 
     def test_quadrature_points(self):
-        obs = irt.quadrature_points(10)
-        exp = np.array([1.33830226e-04,   3.15601632e-03,   3.37736510e-02,
-                        1.64010075e-01,   3.61423830e-01,   3.61423830e-01,
-                        1.64010075e-01,   3.37736510e-02,   3.15601632e-03,
-                        1.33830226e-04])
-        self.assertEqual(obs, exp)
+        theta = irt.quadrature_points(10)
+        self.assertEqual(theta[0], -4.)
+        self.assertAlmostEqual(theta[5], 0.4444, 4)
+        self.assertEqual(theta[9], 4.)
+
+    def test_quadrature_weights(self):
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, 0, 1)
+        self.assertAlmostEqual(theta_w[0], 0.00013, 5)
+        self.assertAlmostEqual(theta_w[5], 0.36142, 5)
+        self.assertAlmostEqual(theta_w[9], 0.00013, 5)
+
+    def test_p_correct(self):
+        self.assertEqual(irt.p_correct(0, 1, 0), 0.5)
+        self.assertEqual(irt.p_correct(2, 1, 2), 0.5)
+        self.assertEqual(irt.p_correct(2, 1, 1), 1. / (1 + np.exp(-1)))
+
+    def test_likelihood(self):
+        theta = irt.quadrature_points(10)
+        a = np.repeat(1.0, 5)
+        b = np.array([-2.155, -0.245, 0.206, 0.984, 1.211])
+        x = np.array([1, 1, 0, 0, 0])
+        L = irt.likelihood(theta, a, b, x)
+        self.assertAlmostEqual(L[0], 0.0030369, 5)
+        self.assertAlmostEqual(L[5], 0.1178053, 5)
+        self.assertAlmostEqual(L[9], 0.0000586, 5)
+
+    def test_eap(self):
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, 0, 1)
+        a = np.repeat(1.0, 5)
+        b = np.array([-2.155, -0.245, 0.206, 0.984, 1.211])
+        x = np.array([1, 1, 0, 0, 0])
+        L = irt.likelihood(theta, a, b, x)
+        theta_hat = irt.eap(theta, L, theta_w)
+        self.assertAlmostEqual(theta_hat, -0.2271, 4)
+
+    def test_eap_psd(self):
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, 0, 1)
+        a = np.repeat(1.0, 5)
+        b = np.array([-2.155, -0.245, 0.206, 0.984, 1.211])
+        x = np.array([1, 1, 0, 0, 0])
+        L = irt.likelihood(theta, a, b, x)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+        self.assertAlmostEqual(theta_psd, 0.7291, 4)
+
+    def test_successive_responses(self):
+        # Step 1
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, 0, 1)
+        L = irt.likelihood(theta, 1., -2.155, 1)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+
+        # Step 2
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, theta_hat, theta_psd)
+        L = irt.likelihood(theta, 1., -0.245, 1)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+
+        # Step 3
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, theta_hat, theta_psd)
+        L = irt.likelihood(theta, 1., 0.206, 0)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+
+        # Step 4
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, theta_hat, theta_psd)
+        L = irt.likelihood(theta, 1., 0.984, 0)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+
+        # Step 5
+        theta = irt.quadrature_points(10)
+        theta_w = irt.quadrature_weights(theta, theta_hat, theta_psd)
+        L = irt.likelihood(theta, 1., 1.211, 0)
+        theta_hat = irt.eap(theta, L, theta_w)
+        theta_psd = irt.eap_psd(theta_hat, theta, L, theta_w)
+
+        self.assertAlmostEqual(theta_hat, -0.23, 2)  # Similar to above, not identical..
+        self.assertAlmostEqual(theta_psd, 0.74, 2)   # Similar to above, not identical..
 
 
+
+
+
+if __name__ == '__main__':
+    unittest.main()
